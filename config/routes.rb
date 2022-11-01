216d132951719442aeafa8c6e1fd019da00d1207
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-01 15:24:21 +0300

    Added seed data

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   devise_scope :user do 
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  namespace :api do
+    namespace :v1 do
+      resources :developers
+      resources :bookings
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
