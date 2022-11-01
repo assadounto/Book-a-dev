af50a9c3c3e2392211bfe85e5e27668fae59217d
Said Rasinlic <saidrasinlic.jr@gmail.com>
2022-11-01 14:01:47 +0100

    Add create method and permit params

--- app/controllers/api/v1/developers_controller.rb
+++ app/controllers/api/v1/developers_controller.rb
@@ -1,7 +1,21 @@
 class Api::V1::DevelopersController < ApplicationController
-   
-    def index
-        @developers = Developer.all
-        render json: @developers
+  def index
+    @developers = Developer.all
+    render json: @developers
+  end
+
+  def create
+    developer = Developer.new(developer_params)
+    if developer.save
+      render json: developer
+    else
+      render json: { error: developer.errors.messages }, status: 422
     end
+  end
+
+  private
+
+  def developer_params
+    params.require(:developer).permit(:name, :photo, :email, :phone, :city, :bio, :github)
+  end
 end
