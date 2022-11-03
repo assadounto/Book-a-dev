49ad51ff0db79d9c8537dd88993dc3c1d7a3f4fc
Grayce Muthui <graycemuthui20@gmail.com>
2022-11-03 11:35:46 +0300

    Add booking

---
+++ app/javascript/components/redux/bookings.js
@@ -0,0 +1,32 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  bookings: [],
+};
+
+export const fetchBookings = createAsyncThunk(
+  "bookings/fetchBookings",
+  async () => {
+    const response = await fetch("/api/v1/bookings");
+    const data = await response.json();
+    return data;
+  }
+);
+
+export const bookingsSlice = createSlice({
+  name: "bookings",
+  initialState,
+  reducers: {
+    addABooking: (state, action) => {
+      state.bookings.push(action.payload);
+    },
+  },
+  extraReducers: {
+    [fetchBookings.fulfilled]: (state, action) => {
+      state.bookings = action.payload;
+    },
+  },
+});
+
+export default bookingsSlice.reducer;
+export const { addABooking } = bookingsSlice.actions;
