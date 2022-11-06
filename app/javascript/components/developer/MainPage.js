07f78f16e2120d7a75ef636c17b220c3035ece65
Grayce Muthui <graycemuthui20@gmail.com>
2022-11-06 17:29:10 +0300

    Add icons

--- app/javascript/components/developer/MainPage.js
+++ app/javascript/components/developer/MainPage.js
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { BsFacebook } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchDevelopers } from "../redux/developer";
 
@@ -17,8 +20,19 @@ function MainPage() {
           <img src={developer.photo} alt="new" className="image" />
         </div>
         <div className="dev-info">
-          <p>{developer.name}</p>
+          <h2>{developer.name}</h2>
           <p>{developer.bio}</p>
+          <div className="icons">
+            <Link>
+              <BsFacebook className="icon-items" />
+            </Link>
+            <Link>
+              <BsTwitter className="icon-items" />
+            </Link>
+            <Link>
+              <BsInstagram className="icon-items" />
+            </Link>
+          </div>
           <Link to={`/developers/${developer.id}`}>Show</Link>
         </div>
       </div>
@@ -27,7 +41,10 @@ function MainPage() {
 
   return (
     <div>
-      <h1 className="mainpage-heading">Developers</h1>
+      <div className="mainpage-heading">
+        <h1>Developers</h1>
+        <p className="main-para">Please select a developer of your choice.</p>
+      </div>
       <div className="display-dev">{displayDevelopers}</div>
       <Link to="/bookings">My Booked Developers</Link>
     </div>
