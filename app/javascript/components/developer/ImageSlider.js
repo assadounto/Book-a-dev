21cf307b4cdeebb383496a25714f27118ab3cbd3
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-11-08 11:06:26 +0000

    Add resolution to dev

--- app/javascript/components/developer/ImageSlider.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-const ImageSlider = ({ slides }) => {
-    const [currentIndex, setCurrentUser] = useState(0);
-    return <div>uuuuuuuu</div>;
-};
-
-export default ImageSlider;
