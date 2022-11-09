4a1f9c7818950390b99a97abd62d2d46d0961744
CrystallineButterfly <95975209+CrystallineButterfly@users.noreply.github.com>
2022-11-09 12:36:29 +0000

    Add basic styling for booking

--- app/javascript/components/booking/AddBooking.js
+++ app/javascript/components/booking/AddBooking.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+// import { code } from "./assets/images/code.jpg";
 
 function AddBooking() {
   const params = useParams();
@@ -7,48 +8,47 @@ function AddBooking() {
   const [time, setTime] = useState("");
   const [city, setCity] = useState("");
   return (
-    <div>
-      AddBooking
-      <div>
-        <label htmlFor="date">Date</label>
-        <input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-        <label htmlFor="time">Time</label>
-        <input
-          type="time"
-          value={time}
-          onChange={(e) => setTime(e.target.value)}
-        />
-        <label htmlFor="city">City</label>
-        <input
-          type="text"
-          value={city}
-          onChange={(e) => setCity(e.target.value)}
-        />
-        <button
-          onClick={(e) => {
-            e.preventDefault();
-            fetch("/api/v1/bookings", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                date: date,
-                time: time,
-                city: city,
-                developer_id: Number(params.id),
-              }),
-            });
-          }}
-        >
-          Add Booking
-        </button>
-      </div>
-    </div>
+        <div className="booking-form">
+          <div className="booking-titles">
+            <label htmlFor="date">Date</label>
+            <input
+              type="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+            <label htmlFor="time">Time</label>
+            <input
+              type="time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            />
+            <label htmlFor="city">City</label>
+            <input
+              type="text"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                fetch("/api/v1/bookings", {
+                  method: "POST",
+                  headers: {
+                    "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify({
+                    date: date,
+                    time: time,
+                    city: city,
+                    developer_id: Number(params.id),
+                  }),
+                });
+              }}
+            >
+              Add Booking
+            </button>
+          </div>
+        </div>
   );
 }
 
