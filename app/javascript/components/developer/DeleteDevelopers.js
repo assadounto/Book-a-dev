9eacd74dd7a0a0a760cdb034d62576c082a2829d
MICHAELMUNAVU83 <michaelmunavu83@gmail.com>
2022-11-09 09:59:25 +0300

    Added delete devs component

--- app/javascript/components/developer/DeleteDevelopers.js
+++ app/javascript/components/developer/DeleteDevelopers.js
@@ -1,17 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchDevelopers } from "../redux/developer";
 
 function DeleteDevelopers() {
   const dispatch = useDispatch();
+  const [fetchedDevelopers, setFetchedDevelopers] = useState([]);
   const developers = useSelector((state) => state.developers.developers);
   useEffect(() => {
     dispatch(fetchDevelopers());
-  }, [dispatch]);
+    setFetchedDevelopers(developers);
+  }, []);
   console.log(developers);
-  const displayDevelopers = developers.map((developer) => (
+  const displayDevelopers = fetchedDevelopers.map((developer) => (
     <div key={developer.id}>
       <p>{developer.name}</p>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          fetch(`/api/v1/developers/${developer.id}`, {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              setFetchedDevelopers(data);
+            });
+        }}
+      >
+        Delete
+      </button>
     </div>
   ));
 
