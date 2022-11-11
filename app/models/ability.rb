620d5f06f27b57d1ed388bcca837213c682ae608
assadounto <adukyerer@gmail.com>
2022-11-11 12:18:56 +0000

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   def initialize(user)
     # Define abilities for the user here. For example:
     return unless user.present?
+
     can :create, Booking
     can :destroy, Booking, user_id: user.id
     can :read, :all
